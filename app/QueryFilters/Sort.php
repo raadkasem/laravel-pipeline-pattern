4de192cdfb5aa0fcfb9636a99911573c005737c7
raadkasem <rkasem0@gmail.com>
2022-09-21 14:49:00 +0300

    Add Queryfilers classes and  implementing pipeline on post controller

---
+++ app/QueryFilters/Sort.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\QueryFilters;
+
+use Closure;
+
+class Sort
+{
+    public function handle($request, Closure $next)
+    {
+        if(! request()->has('sort')){
+            return $next($request);
+        }
+        $builder = $next($request);
+
+        return $builder->orderBy('title', request('sort'));
+    }
+}
