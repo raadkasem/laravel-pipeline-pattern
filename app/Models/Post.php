4dc21ab6cf7a370fbca6d789ee2f7fd7646f0033
raadkasem <rkasem0@gmail.com>
2022-09-21 15:21:35 +0300

    Refactoring Model & Controller

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pipeline\Pipeline;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public static function allPosts()
+    {
+       return app(Pipeline::class)
+            ->send(Post::query())
+            ->through([
+                \App\QueryFilters\Active::class,
+                \App\QueryFilters\Sort::class,
+            ])
+            ->thenReturn()
+            ->get();
+    }
 }
